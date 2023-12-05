1da24463a1ea4bfb663fb5cd36a43253839e5e6b
Anatoliy Topchiev <tolik_t89@mail.ru>
2023-12-05 11:43:41 +0300

    add about me

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Цель: Получить новые знания и научиться их применять.<br />
-        Чем хочу овладеть: Знаниями и навыками по front-end<br />
-        Чем владею: Siebel CRM, PL/SQL<br />
-        О себе: Топчиёв Анатолий, 15.07.1991г.р. Работаю в Ak Bars Digital. 
+        Цель: Получить новые знания и научиться их применять.<br/>
+        Чем хочу овладеть: Знаниями и навыками по front-end<br/>
+        Чем владею: Siebel CRM, PL/SQL<br/>
+        О себе: Топчиёв Анатолий, 15.07.1991г.р. Работаю в Ak Bars Digital.  
         </p>
       </header>
     </div>
